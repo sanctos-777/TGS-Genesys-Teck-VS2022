f1a413297f590bc4327eb455d590dd3654d96a93
sanctos-777 <tiago23032914@gmail.com>
2024-07-30 16:20:28 -0300

    op 30

--- TGS Genesys Teck/obj/Debug/net8.0/TGS Genesys Teck.AssemblyInfo.cs
+++ TGS Genesys Teck/obj/Debug/net8.0/TGS Genesys Teck.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TGS Genesys Teck")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cca147ce862e089c71164fcfc024c47aa4e67254")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f32d3d9011eab0fbb878829dbfd9c797b2d73ba9")]
 [assembly: System.Reflection.AssemblyProductAttribute("TGS Genesys Teck")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TGS Genesys Teck")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
